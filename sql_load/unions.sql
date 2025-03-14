adf3d21c0d0d00286176ec3a156f5e8e06ce0c34
elephanTea4 <rodwb2@naver.com>
2025-03-14 21:43:55 +0000

    first commit

---
+++ sql_load/unions.sql
@@ -0,0 +1,25 @@
+SELECT
+    job_title_short,
+    company_id,
+    job_location
+FROM
+    january_jobs
+
+UNION ALL
+
+SELECT
+    job_title_short,
+    company_id,
+    job_location
+FROM
+    feburary_jobs
+
+
+UNION ALL
+
+SELECT
+    job_title_short,
+    company_id,
+    job_location
+FROM
+    march_jobs
